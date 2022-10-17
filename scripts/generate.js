474bf06e6fb2355e5d4892f7a6b82853e4f09c8b
Mitchell Simpson <git@commitchell.io>
2022-10-17 13:16:34 +0100

    Update theme to newest spec, remove unnecessary theme generation

--- scripts/generate.js
+++ scripts/generate.js
@@ -30,32 +30,6 @@ const withAlphaType = new Type('!alpha', {
 
 const schema = DEFAULT_SCHEMA.extend([withAlphaType]);
 
-/**
- * Soft variant transform.
- * @type {ThemeTransform}
- */
-const transformSoft = (theme) => {
-    /** @type {Theme} */
-    const soft = JSON.parse(JSON.stringify(theme));
-    const brightColors = [...soft.dracula.brightOther];
-    for (const key of Object.keys(soft.colors)) {
-        if (brightColors.includes(soft.colors[key])) {
-            soft.colors[key] = tinycolor(soft.colors[key])
-                .desaturate(20)
-                .toHexString();
-        }
-    }
-    soft.tokenColors = soft.tokenColors.map((value) => {
-        if (brightColors.includes(value.settings.foreground)) {
-            value.settings.foreground = tinycolor(value.settings.foreground)
-                .desaturate(20)
-                .toHexString();
-        }
-        return value;
-    });
-    return soft;
-};
-
 module.exports = async () => {
     const yamlFile = await readFile(
         join(__dirname, '..', 'src', 'midnight.yml'),
@@ -72,8 +46,5 @@ module.exports = async () => {
         }
     }
 
-    return {
-        base,
-        soft: transformSoft(base),
-    };
+    return { base };
 };
